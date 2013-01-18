9a522fe43c58877d46f8072eb4cf47dc4d6b27e1
Jorgen Lundman <lundman@lundman.net>
2013-01-18 12:27:22 +0900

    Add GCM cipher, with GMAC. Fix CCM, GCM and CTR to use correct iv param. Add CTR cipher for ZVOL.

--- sys/cddl/compat/opensolaris/kern/opensolaris_crypto.c
+++ sys/cddl/compat/opensolaris/kern/opensolaris_crypto.c
@@ -4,6 +4,9 @@
 #include <sys/mbuf.h>
 #include <crypto/rijndael/rijndael.h>
 #include <sys/sun_ccm.h>
+#include <sys/sun_gcm.h>
+#include <crypto/sha2/sha2.h>
+#include <sys/mbuf.h>
 
 
 // ZFS_CRYPTO_VERBOSE is set in the crypto/api.h file
@@ -28,12 +31,35 @@ enum cipher_type_t {
     CIPHER_TYPE_MAC,
 };
 
+enum param_type_t {
+    PARAM_TYPE_NONE = 0,
+    PARAM_TYPE_CCM,
+    PARAM_TYPE_GCM,
+    PARAM_TYPE_CTR
+};
+
 struct cipher_map_s {
     enum cipher_type_t type;
+    enum param_type_t param_type;
     char *solaris_name;
-    int power_on_test; /* If 0, check cipher exists. Set to 1 after that */
-    char *freebsd_name;
-    char *hmac_name;   /* optional hmac if not part of linux_name */
+
+    void (*setkey)(rijndael_ctx *, uint8_t *, uint32_t);
+
+    int (*enc)(rijndael_ctx *,
+               struct mbuf *,
+               struct mbuf *,
+               uint64_t,
+               uint8_t *,
+               uint32_t,
+               uint32_t);
+
+    int (*dec)(rijndael_ctx *,
+               struct mbuf *,
+               struct mbuf *,
+               uint64_t,
+               uint8_t *,
+               uint32_t,
+               uint32_t);
 };
 
 typedef struct cipher_map_s cipher_map_t;
@@ -41,16 +67,18 @@ typedef struct cipher_map_s cipher_map_t;
 static cipher_map_t cipher_map[] =
 {
     /* 0, not used, must be defined */
-    { CIPHER_TYPE_MAC,  "NULL Cipher", 0, NULL, NULL },
-#if 0
-    // TODO, attempt to make the MAC be the same as Solaris
-    { CIPHER_TYPE_STREAM, "CKM_AES_CCM", 0, "sun-ctr(aes)", "hmac(sha256)" },
-#else
-    { CIPHER_TYPE_STREAM, "CKM_AES_CCM", 0, "sun-ccm(aes)", NULL },
-#endif
-    { CIPHER_TYPE_STREAM, "CKM_AES_GCM", 0, "sun-gcm(aes)", NULL },
-    { CIPHER_TYPE_BLOCK,  "CKM_AES_CTR", 0, "sun-ctr(aes)", NULL },
-    { CIPHER_TYPE_MAC,  "CKM_SHA256_HMAC_GENERAL", 0, NULL, "hmac(sha256)" },
+    { CIPHER_TYPE_MAC, PARAM_TYPE_NONE, "NULL Cipher", NULL, NULL, NULL },
+
+    { CIPHER_TYPE_STREAM, PARAM_TYPE_CCM, "CKM_AES_CCM",
+      sun_ccm_setkey, sun_ccm_encrypt_and_auth, sun_ccm_encrypt_and_auth },
+    { CIPHER_TYPE_STREAM, PARAM_TYPE_GCM, "CKM_AES_GCM",
+      sun_gcm_setkey, sun_gcm_encrypt_and_auth, sun_gcm_decrypt_and_auth },
+
+    { CIPHER_TYPE_BLOCK, PARAM_TYPE_GCM, "CKM_AES_CTR",
+      sun_gcm_setkey, sun_gcm_encrypt_and_auth, sun_gcm_decrypt_and_auth },
+
+    { CIPHER_TYPE_MAC, PARAM_TYPE_NONE, "CKM_SHA256_HMAC_GENERAL",
+      NULL, NULL, NULL },
 };
 
 #define NUM_CIPHER_MAP (sizeof(cipher_map) / sizeof(cipher_map_t))
@@ -65,22 +93,6 @@ static void free_function(void *buf, void *arg)
     return;
 }
 
-
-
-
-int crypto_mac(crypto_mechanism_t *mech, crypto_data_t *data,
-               crypto_key_t *key, crypto_ctx_template_t tmpl, crypto_data_t *mac,
-               crypto_call_req_t *cr)
-{
-    int ret = CRYPTO_FAILED;
-
-    printf("crypto_mac\n");
-
-#ifdef ZFS_CRYPTO_VERBOSE
-    printf("spl-crypto: mac returning %d\n", ret);
-#endif
-    return ret;
-}
 
 
 
@@ -167,6 +179,119 @@ static size_t crypto_map_buffers(crypto_data_t *solaris_buffer,
 }
 
 
+static int spl_crypto_map_iv(unsigned char *iv, int len,
+                             crypto_mechanism_t *mech)
+{
+    cipher_map_t *cm = NULL;
+
+    // Make sure we are to use iv
+    if (!mech || !mech->cm_param || (len < 16)) goto clear;
+
+    cm = &cipher_map[ mech->cm_type ];
+
+    switch(cm->param_type) {
+
+    case PARAM_TYPE_CCM:
+        {
+            CK_AES_CCM_PARAMS *ccm_param = (CK_AES_CCM_PARAMS *)mech->cm_param;
+            if (!ccm_param || !ccm_param->nonce) goto clear;
+
+            memcpy(iv, ccm_param->nonce, ccm_param->ulNonceSize);
+            return ccm_param->ulNonceSize;
+
+        }
+        break;
+
+
+    case PARAM_TYPE_GCM:
+        {
+            CK_AES_GCM_PARAMS *gcm_param = (CK_AES_GCM_PARAMS *)mech->cm_param;
+            uint32_t ivlen;
+            if (!gcm_param || !gcm_param->pIv) goto clear;
+
+            /*
+             * Unfortunately, the implementations between FreeBSD and
+             * Linux differ in handling the case of GCM ivlen != 12.
+             * So we force ivlen = 12 for now.
+             */
+
+            ivlen = gcm_param->ulIvLen;
+            if (ivlen != 12) ivlen = 12;
+
+            memset(iv, 0, len);
+            memcpy(iv, gcm_param->pIv, MIN(gcm_param->ulIvLen, ivlen));
+
+            return 12;
+        }
+        break;
+
+    case PARAM_TYPE_CTR:
+        {
+            CK_AES_CTR_PARAMS *ctr_param = (CK_AES_CTR_PARAMS *)mech->cm_param;
+            if (!ctr_param) goto clear;
+
+            memset(iv, 0, sizeof(iv));
+            memcpy(iv, ctr_param->cb, ctr_param->ulCounterBits >> 3);
+            /* Linux crypto API does not let you change ivlen */
+            //return ctr_param->ulCounterBits >> 3;
+            return 16;
+        }
+
+    default:
+        break;
+    }
+
+ clear:
+    memset(iv, 0, len);
+    return 0;
+}
+
+
+int crypto_mac(crypto_mechanism_t *mech, crypto_data_t *data,
+               crypto_key_t *key, crypto_ctx_template_t tmpl,
+               crypto_data_t *mac, crypto_call_req_t *cr)
+{
+    int ret = CRYPTO_FAILED;
+    SHA256_CTX sha;
+    u_int8_t digest[SHA256_DIGEST_LENGTH];
+    struct mbuf *mdata  = NULL;
+    struct mbuf *mmac  = NULL;
+    size_t plainlen = 0, cryptlen = 0;
+
+    // Setup the first buffer
+    if (!(plainlen  = crypto_map_buffers(data,  &mdata)))
+        goto out;
+    if (!(cryptlen  = crypto_map_buffers(mac, &mmac)))
+        goto out;
+
+    SHA256_Init(&sha);
+    while(mdata && plainlen) {
+        SHA256_Update(&sha, mtod(mdata, uint8_t *), mdata->m_len);
+        mdata = mdata->m_next;
+    }
+    SHA256_Final(digest, &sha);
+
+    // FIXME, doesn't handle split digest buffer
+    if (mmac && (mmac->m_len >= SHA256_DIGEST_LENGTH)) {
+        memcpy(mtod(mmac, void *), digest, SHA256_DIGEST_LENGTH);
+    }
+
+    ret = CRYPTO_SUCCESS;
+
+    m_freem(mdata);
+    m_freem(mmac);
+
+ out:
+#ifdef ZFS_CRYPTO_VERBOSE
+    printf("spl-crypto: mac returning %d\n", ret);
+#endif
+    return ret;
+}
+
+
+
+
+
 
 static int crypto_encrypt_stream(crypto_mechanism_t *mech,
                                  crypto_data_t *plaintext,
@@ -179,12 +304,14 @@ static int crypto_encrypt_stream(crypto_mechanism_t *mech,
     size_t plainlen = 0, cryptlen = 0, maclen = 0;
     rijndael_ctx   cc_aes;
     int ret;
-    CK_AES_CCM_PARAMS *ccm_param;
+    cipher_map_t *cm = NULL;
+    uint8_t iv[16];
+    uint32_t ivlen = 0;
 
     ASSERT(mech != NULL);
 
-    ccm_param = (CK_AES_CCM_PARAMS *)mech->cm_param;
-    ASSERT(ccm_param != NULL);
+    if (mech->cm_type >= NUM_CIPHER_MAP) return CRYPTO_FAILED;
+    cm = &cipher_map[ mech->cm_type ];
 
 #ifdef ZFS_CRYPTO_VERBOSE
     printf("crypto_encrypt_stream: %04lx\n", plaintext->cd_length);
@@ -200,15 +327,21 @@ static int crypto_encrypt_stream(crypto_mechanism_t *mech,
 
     maclen = cryptlen - plainlen;
 
-    sun_ccm_setkey(&cc_aes, key->ck_data, key->ck_length / 8);
+    // in CTR mode, we have no authtag
+    if (cm->param_type == PARAM_TYPE_CTR)
+        maclen = 0;
 
-    ret = sun_ccm_encrypt_and_auth(&cc_aes,
-                                   mplain,
-                                   mcipher,
-                                   plainlen,
-                                   ccm_param->nonce,
-                                   ccm_param->ulNonceSize,
-                                   maclen);
+    cm->setkey(&cc_aes, key->ck_data, key->ck_length / 8);
+
+    ivlen = spl_crypto_map_iv(iv, sizeof(iv), mech);
+
+    ret = cm->enc(&cc_aes,
+                  mplain,
+                  mcipher,
+                  plainlen,
+                  iv,
+                  ivlen,
+                  maclen);
 
 #ifdef ZFS_CRYPTO_VERBOSE
     printf("crypto_encrypt_stream: result %d\n",
@@ -224,6 +357,8 @@ static int crypto_encrypt_stream(crypto_mechanism_t *mech,
     return CRYPTO_FAILED;
 }
 
+
+
 static int crypto_decrypt_stream(crypto_mechanism_t *mech,
                                  crypto_data_t *ciphertext,
                                  crypto_key_t *key, crypto_ctx_template_t tmpl,
@@ -235,12 +370,14 @@ static int crypto_decrypt_stream(crypto_mechanism_t *mech,
     size_t plainlen = 0, cryptlen = 0, maclen = 0;
     rijndael_ctx   cc_aes;
     int ret;
-    CK_AES_CCM_PARAMS *ccm_param;
+    uint8_t iv[16];
+    uint32_t ivlen = 0;
+    cipher_map_t *cm = NULL;
 
     ASSERT(mech != NULL);
 
-    ccm_param = (CK_AES_CCM_PARAMS *)mech->cm_param;
-    ASSERT(ccm_param != NULL);
+    if (mech->cm_type >= NUM_CIPHER_MAP) return CRYPTO_FAILED;
+    cm = &cipher_map[ mech->cm_type ];
 
 #ifdef ZFS_CRYPTO_VERBOSE
     printf("crypto_decrypt_stream: %04lx\n", plaintext->cd_length);
@@ -256,15 +393,20 @@ static int crypto_decrypt_stream(crypto_mechanism_t *mech,
 
     maclen = cryptlen - plainlen;
 
-    sun_ccm_setkey(&cc_aes, key->ck_data, key->ck_length / 8);
+    if (cm->param_type == PARAM_TYPE_CTR)
+        maclen = 0;
 
-    ret = sun_ccm_decrypt_and_auth(&cc_aes,
-                                   mcipher,
-                                   mplain,
-                                   plainlen,
-                                   ccm_param->nonce,
-                                   ccm_param->ulNonceSize,
-                                   maclen);
+    cm->setkey(&cc_aes, key->ck_data, key->ck_length / 8);
+
+    ivlen = spl_crypto_map_iv(iv, sizeof(iv), mech);
+
+    ret = cm->dec(&cc_aes,
+                  mcipher,
+                  mplain,
+                  plainlen,
+                  iv,
+                  ivlen,
+                  maclen);
 
 #ifdef ZFS_CRYPTO_VERBOSE
     printf("crypto_decrypt_stream: result %d\n",
@@ -291,8 +433,12 @@ static int crypto_encrypt_block(crypto_mechanism_t *mech,
                                 crypto_data_t *ciphertext,
                                 crypto_call_req_t *cr)
 {
-    printf("Uhoh, no BLOCK encrypt method\n");
-    return CRYPTO_FAILED;
+    int ret;
+
+    ret = crypto_encrypt_stream(mech, plaintext, key, tmpl,
+                                ciphertext, cr);
+
+    return ret;
 }
 
 static int crypto_decrypt_block(crypto_mechanism_t *mech,
@@ -301,8 +447,15 @@ static int crypto_decrypt_block(crypto_mechanism_t *mech,
                                 crypto_data_t *plaintext,
                                 crypto_call_req_t *cr)
 {
-    printf("Uhoh, no BLOCK decrypt method\n");
-    return CRYPTO_FAILED;
+    int ret;
+
+    ret = crypto_decrypt_stream(mech, ciphertext, key, tmpl,
+                                plaintext, cr);
+
+    // No authtag used with plain CTR
+    if (ret == EBADMSG) ret = CRYPTO_SUCCESS;
+
+    return ret;
 }
 
 
@@ -320,15 +473,17 @@ int crypto_encrypt(crypto_mechanism_t *mech, crypto_data_t *plaintext,
     if (mech->cm_type >= NUM_CIPHER_MAP) return CRYPTO_FAILED;
     cm = &cipher_map[ mech->cm_type ];
 
-#if 1
-    if (cm->type == CIPHER_TYPE_STREAM)
+    switch(cm->type) {
+    case CIPHER_TYPE_STREAM:
         return crypto_encrypt_stream(mech, plaintext, key, tmpl,
                                      ciphertext, cr);
-
-    if (cm->type == CIPHER_TYPE_BLOCK)
+    case CIPHER_TYPE_BLOCK:
         return crypto_encrypt_block(mech, plaintext, key, tmpl,
                                     ciphertext, cr);
-#endif
+    case CIPHER_TYPE_MAC:
+        return crypto_mac(mech, plaintext, key, tmpl,
+                          ciphertext, cr);
+    }
 
     return CRYPTO_FAILED;
 }
@@ -346,15 +501,18 @@ int crypto_decrypt(crypto_mechanism_t *mech, crypto_data_t *ciphertext,
     if (mech->cm_type >= NUM_CIPHER_MAP) return CRYPTO_FAILED;
     cm = &cipher_map[ mech->cm_type ];
 
-#if 1
-    if (cm->type == CIPHER_TYPE_STREAM)
+    switch(cm->type) {
+    case CIPHER_TYPE_STREAM:
         return crypto_decrypt_stream(mech, ciphertext, key, tmpl,
                                      plaintext, cr);
 
-    if (cm->type == CIPHER_TYPE_BLOCK)
+    case CIPHER_TYPE_BLOCK:
         return crypto_decrypt_block(mech, ciphertext, key, tmpl,
                                     plaintext, cr);
-#endif
+    case CIPHER_TYPE_MAC:
+        return crypto_mac(mech, plaintext, key, tmpl,
+                          ciphertext, cr);
+    }
 
     return CRYPTO_FAILED;
 }
@@ -389,38 +547,21 @@ crypto_mech_type_t crypto_mech2id(crypto_mech_name_t name)
     if (!name || !*name)
         return CRYPTO_MECH_INVALID;
 
-#ifdef ZFS_CRYPTO_VERBOSE
-    printf("called crypto_mech2id '%s' (total %d)\n", name, (int)NUM_CIPHER_MAP);
-#endif
-
     for (i = 0; i < NUM_CIPHER_MAP; i++) {
 
         if (cipher_map[i].solaris_name &&
             !strcmp(cipher_map[i].solaris_name, name)) {
 
-            // Do we test the cipher?
-            if (!cipher_map[i].power_on_test) {
-
-                // Test it only once
-                cipher_map[i].power_on_test = 1;
-
-                if (cipher_map[i].type == CIPHER_TYPE_STREAM) {
-
-                } else if (cipher_map[i].type == CIPHER_TYPE_BLOCK) {
-
-                } else if (cipher_map[i].type == CIPHER_TYPE_MAC) {
-
-                } else {
-                    return CRYPTO_MECH_INVALID;
-                }
-
-            }
+#ifdef ZFS_CRYPTO_VERBOSE
+            printf("called crypto_mech2id '%s' (returning %d)\n",
+                   name, i);
+#endif
 
             return i; // Index into list.
         }
     } // for all cipher maps
 
-    printf("spl-crypto: mac2id returning INVALID\n");
+    printf("spl-crypto: mac2id returning INVALID for '%s'\n", name);
     return CRYPTO_MECH_INVALID;
 }
 
